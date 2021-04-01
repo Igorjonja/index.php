9414f68d79f23031918830f23957d63ea4da94ba
Igorjonja <igor.yatsenko92l@gmail.com>
2021-04-01 20:32:54 +0300

    prepairing functions and data

--- AtomCMS/config/setup.php
+++ AtomCMS/config/setup.php
@@ -4,7 +4,22 @@
 # Database connection here
 $dbc = mysqli_connect('localhost','dev','!79eRV2VJcEGgdH','atomcms') or die('Could not connect because: '.mysqli_connect_error());
 
-$page_title = 'AtomCMS';
-$site_title =  'Home Page';
+#Functions:
+include('functions/data.php');
+
+
+
+
+$site_title =  'AtomCMS 2.0';
+
+if(isset($_GET['page'])){
+    $pageid = $_GET['page']; // Set $pageid to equal the value  given in the URL
+}else {
+    $pageid = 1; // Set     $pageid equal to 1 or the Home Page
+}
+
+#Page Setup
+$page = data_page($dbc, $pageid);
+
 
 ?>
--- AtomCMS/index.php
+++ AtomCMS/index.php
@@ -1,11 +1,10 @@
 <?php include ('config/setup.php'); ?>
 <!doctype html>
-<html lang="en">
+<html lang="en" class="h-100">
 <head>
-    <title><?php echo $page_title. ' | ' .$site_title?></title>
+    <title><?php echo $page['title']. ' | ' .$site_title?></title>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <?php include ('config/css.php'); ?>
@@ -13,46 +12,49 @@
     <?php include ('config/js.php'); ?>
 
 </head>
-    <div id="wrap">
-        <nav class="navbar navbar-expand-sm navbar-light bg-light">
-            <div class="container">
+<body class="d-flex flex-column h-100">
+    <main role="main" class="flex-shrink-0">
+        <div id="wrap">
+            <nav class="navbar navbar-expand-sm navbar-light bg-light">
+                <div class="container">
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About Us</a>
-                        </li>
+                        <ul class="navbar-nav mr-auto">
+                            <li<?php if($pageid ==1) {echo ' class="nav-item active"';};?>>
+                                <a class="nav-link" href="?page=1">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li<?php if($pageid ==2) {echo ' class="nav-item active"';} ?>>
+                                <a class="nav-link" href="?page=2">About Us</a>
+                            </li>
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">FAQ</a>
-                        </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">FAQ</a>
+                            </li>
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact</a>
-                        </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact</a>
+                            </li>
 
-                    </ul>
+                        </ul>
+                    </div>
                 </div>
+
+            </nav> <!--END main navigation-->
+
+
+
+
+
+            <div class="container">
+                <h1><?php echo $page['header']; ?> </h1>
+                <p><?php echo $page['body']; ?> </p>
+
             </div>
-
-        </nav> <!--END main navigation-->
-
-
-
-
-
-        <div class="container">
-            <h1>Hello World!
-            </h1>
 
         </div>
 
-    </div>
-
+    </main>
 
     <footer  class="footer mt-auto py-3" id="footer">
         <div class="container">
